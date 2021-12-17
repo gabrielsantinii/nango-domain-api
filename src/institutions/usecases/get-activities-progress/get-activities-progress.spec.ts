300b6443b252398a98fa04a033977dbf4e223577
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-17 00:34:04 -0300

    refactor: make-sut for get-activities-progress

--- src/institutions/usecases/get-activities-progress/get-activities-progress.spec.ts
+++ src/institutions/usecases/get-activities-progress/get-activities-progress.spec.ts
@@ -1,17 +1,27 @@
+
+
 interface ActivitiesProgress {
     finishedCount: number;
     totalCount: number;
 }
 
-class GetActivitiesProgress {
+interface GetActivitiesProgressResult {
+    perform: () => ActivitiesProgress
+}
+
+class GetActivitiesProgress implements GetActivitiesProgressResult {
     perform(): ActivitiesProgress {
         return { finishedCount: 0, totalCount: 1 };
     }
 }
 
+const makeSut = (): GetActivitiesProgressResult => {
+    return new GetActivitiesProgress()
+}
+
 describe("get activities progress need to return the totalCount and finishedCount", () => {
     it("the total count must be greater or equal than finishedCount", () => {
-        const sut = new GetActivitiesProgress();
+        const sut = makeSut()
         const { finishedCount, totalCount } = sut.perform();
         expect(totalCount).toBeGreaterThanOrEqual(finishedCount);
     });
