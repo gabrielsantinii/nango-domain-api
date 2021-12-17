51e33b85528d057480a6b4bbe49d1b56a0930db5
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-16 22:39:18 -0300

    test: throws error when the date is in the past

--- src/institutions/usecases/get-period-remaining-days/get-period-remaining-days.spec.ts
+++ src/institutions/usecases/get-period-remaining-days/get-period-remaining-days.spec.ts
@@ -1,22 +1,33 @@
-interface GetPeriodEndDate {
-    perform: () => { institutionId: string; periodEndDate: Date };
+import { addDays, differenceInCalendarDays } from "date-fns";
+
+interface GetPeriodRemainingDays {
+    perform: () => { institutionId: string; periodEndDate: Date; remainingDays: number };
 }
 
-class GetPeriodEndDateSpy implements GetPeriodEndDate {
+class GetPeriodRemainingDaysImp implements GetPeriodRemainingDays {
     constructor(private readonly institutionId: string, private readonly addDays: number) {}
 
     perform() {
         const today = new Date();
-        const todayAfterAddDays = today.setDate(today.getDate() + this.addDays);
-        return { institutionId: this.institutionId, periodEndDate: new Date(todayAfterAddDays) };
+        const todayAfterAddDays = addDays(today, this.addDays);
+        const remainingDays = differenceInCalendarDays(todayAfterAddDays, today);
+        const isInFuture = todayAfterAddDays > today;
+        if (!isInFuture) {
+            throw new Error("The period end date is in the past.");
+        }
+        return { institutionId: this.institutionId, periodEndDate: todayAfterAddDays, remainingDays };
     }
 }
 
-describe("the semestral date need to be in the future", () => {
-    it("should return an future date", () => {
-        const getPeriodEndDateSpy = new GetPeriodEndDateSpy("any_institution_id", 5);
-        const { periodEndDate } = getPeriodEndDateSpy.perform();
-        const endDateIsInFuture = periodEndDate > new Date();
-        expect(endDateIsInFuture).toBeTruthy();
+describe("the period date need to be in the future", () => {
+    it("should return an positive number for remaining days", () => {
+        const sut = new GetPeriodRemainingDaysImp("any_institution_id", 5);
+        const { remainingDays } = sut.perform();
+        expect(remainingDays).toBeGreaterThan(0);
+    });
+
+    it("should throw erro cause the remaining days are not positive", () => {
+        const sut = new GetPeriodRemainingDaysImp("any_institution_id", 0);
+        expect(() => sut.perform()).toThrow("The period end date is in the past.");
     });
 });
