389a1d55a42f6a601f9398c730579c9ba439f493
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-17 01:17:48 -0300

    chore: renamed interface

--- src/institutions/interfaces/index.ts
+++ src/institutions/interfaces/index.ts
@@ -1,4 +1,4 @@
 export * from "./get-period-remaining-days.interface";
-export * from "./get-institution-by-id-result.interface";
+export * from "./get-institution-by-id.interface";
 export * from './get-activities-progress.interface'
 export * from './get-activities-by-institution-id.interface'
