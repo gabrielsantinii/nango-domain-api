98b293bd2e5a73bf20e8541497a997234ed37123
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-17 01:59:23 -0300

    test: make-sut factory, interfaces and Spy ensuring the return are list empty or filled, depending on institutionId

--- src/institutions/usecases/get-classes-by-institution-id/get-classes-by-institution-id.spec.ts
+++ src/institutions/usecases/get-classes-by-institution-id/get-classes-by-institution-id.spec.ts
@@ -1,14 +1,38 @@
-class GetClassesByInstitutionId {
+interface ClassByInstitutionId {
+    id: string;
+    institutionId: string;
+}
 
-    async perform(): Promise<{ id: string, institutionId: string }[]> {
-        return []
+interface GetClassesByInstitutionIdResult {
+    perform: () => Promise<ClassByInstitutionId[]>;
+}
+
+class GetClassesByInstitutionId implements GetClassesByInstitutionIdResult {
+    constructor(private readonly institutionId: string) {}
+    private readonly existantClasses: ClassByInstitutionId[] = [{ id: "any", institutionId: "existant_institution" }];
+
+    async perform() {
+        const classes = this.existantClasses.filter((eClass) => eClass.institutionId === this.institutionId);
+        return classes;
     }
 }
+type SutParams = { institutionId: string };
+type SutType = { sut: GetClassesByInstitutionIdResult; institutionId: string };
+const makeSut = ({ institutionId }: SutParams): SutType => {
+    const sut = new GetClassesByInstitutionId(institutionId);
+    return { sut, institutionId };
+};
 
 describe("get-classes-by-institution-id needs to return a list of classes related to institutionId", () => {
     it("should return an empty list of classes once the institution does not have any class configured", async () => {
-        const sut = new GetClassesByInstitutionId()
+        const { sut } = makeSut({ institutionId: "NOT_EXISTS" });
         const classes = await sut.perform();
         expect(classes.length).toBe(0);
+    });
+
+    it("should return a filled list of classes related to the institution", async () => {
+        const { sut } = makeSut({ institutionId: "existant_institution" });
+        const classes = await sut.perform();
+        expect(classes.length).toBeGreaterThanOrEqual(1);
     });
 });
