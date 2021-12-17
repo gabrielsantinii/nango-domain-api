f63c187d59ebb687059ba9a2b3ffaa592361a179
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-16 22:18:18 -0300

    test: period-end-date-spy must return a future date

--- src/institutions/usecases/get-period-remaining-days/get-period-remaining-days.spec.ts
+++ src/institutions/usecases/get-period-remaining-days/get-period-remaining-days.spec.ts
@@ -1,5 +1,22 @@
+interface GetPeriodEndDate {
+    perform: () => { institutionId: string; periodEndDate: Date };
+}
+
+class GetPeriodEndDateSpy implements GetPeriodEndDate {
+    constructor(private readonly institutionId: string, private readonly addDays: number) {}
+
+    perform() {
+        const today = new Date();
+        const todayAfterAddDays = today.setDate(today.getDate() + this.addDays);
+        return { institutionId: this.institutionId, periodEndDate: new Date(todayAfterAddDays) };
+    }
+}
+
 describe("the semestral date need to be in the future", () => {
     it("should return an future date", () => {
-        expect(1).toBe(1);
+        const getPeriodEndDateSpy = new GetPeriodEndDateSpy("any_institution_id", 5);
+        const { periodEndDate } = getPeriodEndDateSpy.perform();
+        const endDateIsInFuture = periodEndDate > new Date();
+        expect(endDateIsInFuture).toBeTruthy();
     });
 });
