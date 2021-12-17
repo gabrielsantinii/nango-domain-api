bfd27bf6a33f3eda87ed25a48bb025f3d9ee213a
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-17 01:41:25 -0300

    test: ensure the students are returned if institutionId exists

--- src/institutions/usecases/get-students-by-institution-id/get-students-by-institution-id.spec.ts
+++ src/institutions/usecases/get-students-by-institution-id/get-students-by-institution-id.spec.ts
@@ -1,13 +1,43 @@
-class GetStudentsByInstitutionId {
-    async perform(): Promise<{ institutionId: string, id: string }[]> {
-        return []
+interface StudentByInstitutionId {
+    institutionId: string;
+    id: string;
+}
+
+interface GetStudentsByInstitutionIdResult {
+    perform: () => Promise<StudentByInstitutionId[]>;
+}
+
+class GetStudentsByInstitutionId implements GetStudentsByInstitutionIdResult {
+    constructor(private readonly institutionId: string) {}
+    private readonly existantsStudents: StudentByInstitutionId[] = [
+        { id: "any", institutionId: "existant_institution" },
+    ];
+    async perform() {
+        const students = this.existantsStudents.filter((student) => student.institutionId === this.institutionId);
+        return students;
     }
 }
 
+type SutParams = { institutionId: string };
+type SutType = { sut: GetStudentsByInstitutionIdResult; institutionId: string };
+
+const makeSut = ({ institutionId }: SutParams): SutType => {
+    const sut = new GetStudentsByInstitutionId(institutionId);
+    return { sut, institutionId };
+};
+
 describe("get-students-by-institution-id must return an list of students related to institutionId", () => {
     it("should return an empty list once dont have any student related", async () => {
-        const sut = new GetStudentsByInstitutionId()
-        const studentsList = await sut.perform()
-        expect(studentsList.length).toBe(0)
-    })
-})
+        const { sut } = makeSut({ institutionId: "NOT_EXISTS" });
+        const studentsList = await sut.perform();
+        expect(studentsList.length).toBe(0);
+    });
+
+    it("should return an filled list of students related to institutionId", async () => {
+        const { sut } = makeSut({ institutionId: "existant_institution" });
+        const studentsList = await sut.perform();
+        expect(studentsList.length).toBeGreaterThanOrEqual(1);
+    });
+});
+{
+}
