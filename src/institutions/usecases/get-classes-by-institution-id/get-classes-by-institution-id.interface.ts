60182fefe655d2772d5f9d2de46fd9a0c9c6e250
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-17 20:06:29 -0300

    fix: class by institution id interface

--- src/institutions/usecases/get-classes-by-institution-id/get-classes-by-institution-id.interface.ts
+++ src/institutions/usecases/get-classes-by-institution-id/get-classes-by-institution-id.interface.ts
@@ -1,6 +1,10 @@
 export interface ClassByInstitutionId {
     id: string;
     institutionId: string;
+    name: string;
+    trailsCount: number;
+    studentsCount: number;
+    educators: { name: string; id: string }[];
 }
 
 export interface GetClassesByInstitutionIdResult {
--- src/institutions/usecases/get-classes-by-institution-id/get-classes-by-institution-id.ts
+++ src/institutions/usecases/get-classes-by-institution-id/get-classes-by-institution-id.ts
@@ -2,7 +2,16 @@ import { ClassByInstitutionId, GetClassesByInstitutionIdResult } from ".";
 
 export class GetClassesByInstitutionId implements GetClassesByInstitutionIdResult {
     constructor(private readonly institutionId: string) {}
-    private readonly existantClasses: ClassByInstitutionId[] = [{ id: "any", institutionId: "existant_institution" }];
+    private readonly existantClasses: ClassByInstitutionId[] = [
+        {
+            id: "any",
+            institutionId: "existant_institution",
+            name: "any-class-name",
+            educators: [{ id: "any_ed_id", name: "any-Name" }],
+            studentsCount: 10,
+            trailsCount: 12,
+        },
+    ];
 
     async perform() {
         const classes = this.existantClasses.filter((eClass) => eClass.institutionId === this.institutionId);
