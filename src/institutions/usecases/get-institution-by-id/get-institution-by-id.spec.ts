98b1e2fa58628389380d724bf8115bf64a2c3f3c
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-16 23:37:23 -0300

    test: get-institution-by-id-spy returns institution record

--- src/institutions/usecases/get-institution-by-id/get-institution-by-id.spec.ts
+++ src/institutions/usecases/get-institution-by-id/get-institution-by-id.spec.ts
@@ -1,17 +1,22 @@
-// class GetInstitutionById implements GetInstitutionByIdResult {
-//     constructor(private readonly institutionId: string) {}
+import { InstitutionById } from "../../interfaces/get-institution-by-id-result.interface";
+interface GetInstitutionByIdResult {
+    perform: () => Promise<InstitutionById>;
+}
 
-//     async perform() {
-//         return {
-//             id: this.institutionId,
-//             periodEndDate: "",
-//             name: "",
-//         };
-//     }
-// }
+class GetInstitutionByIdSpy implements GetInstitutionByIdResult {
+    constructor(private readonly institutionId: string) {}
 
-describe("", () => {
-    it("description", async () => {
-        expect(true).toBeTruthy();
+    async perform() {
+        return { id: this.institutionId, name: "", periodEndDate: new Date() };
+    }
+}
+
+describe("the get institution by id needs to return the institution record", () => {
+    it("should return the same taken id on arguments.", async () => {
+        const sut = new GetInstitutionByIdSpy("anyInstitutionId");
+        const institutionById = await sut.perform();
+        expect(institutionById.id).toBe("anyInstitutionId");
     });
+
+    it("should throws exception for not found institution ID", async () => {});
 });
