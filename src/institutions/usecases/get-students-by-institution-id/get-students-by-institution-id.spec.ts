26ac632822d2d289f2a106a5a2f1a68a60d53790
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-17 01:32:51 -0300

    test: ensure when dont have any student related, return empty list

--- src/institutions/usecases/get-students-by-institution-id/get-students-by-institution-id.spec.ts
+++ src/institutions/usecases/get-students-by-institution-id/get-students-by-institution-id.spec.ts
@@ -1,1 +1,13 @@
+class GetStudentsByInstitutionId {
+    async perform(): Promise<{ institutionId: string, id: string }[]> {
+        return []
+    }
+}
 
+describe("get-students-by-institution-id must return an list of students related to institutionId", () => {
+    it("should return an empty list once dont have any student related", async () => {
+        const sut = new GetStudentsByInstitutionId()
+        const studentsList = await sut.perform()
+        expect(studentsList.length).toBe(0)
+    })
+})
