a3f4e208d95bab4af0e5f0de51a044d080b943f1
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-18 19:46:36 -0300

    feat: institution model ready for create institutions

--- src/data/institutions/models/institution.model.ts
+++ src/data/institutions/models/institution.model.ts
@@ -1,11 +1,35 @@
+import { addMonths } from "date-fns";
 import { Document, Schema, model, Model } from "mongoose";
-import { InstitutionDto } from "../dtos";
+import { Status } from "../../../shared/enums";
+import { ReadInstitutionDto } from "../dtos";
+import { InstitutionCategory } from "../enums";
 
-type IInstitutionModel = InstitutionDto & Document;
+type InstitutionDocument = ReadInstitutionDto & Document;
 
-const InstitutionSchema: Schema<IInstitutionModel> = new Schema();
-const InstitutionModel = model<IInstitutionModel>("institutions", InstitutionSchema);
+const InstitutionSchema: Schema<InstitutionDocument> = new Schema({
+    email: { type: String, required: true },
+    name: { type: String, required: true },
+    website: { type: String },
+    rangeOfStudents: { type: String, required: true },
+    periodEndDate: { type: Date, required: true, default: addMonths(new Date(), 6) },
+    categories: { type: [String], required: true, enum: Object.values(InstitutionCategory) },
 
-type InstitutionModelType = Model<IInstitutionModel>;
+    rangeOfEmployees: { type: String, required: true },
+    photoUrl: { type: String, required: true, default: "" },
+    address: {
+        country: { type: String, required: true },
+        postalCode: { type: String, default: "" },
+        city: { type: String, required: true },
+        street: { type: String, default: "" },
+        state: { type: String, required: true },
+    },
+    createdAt: { type: Date, required: true },
+    updatedAt: { type: Date, required: true },
+    id: { type: String, required: true },
+    status: { type: String, required: true, enum: Object.values(Status) },
+});
+const InstitutionModel = model<InstitutionDocument>("institutions", InstitutionSchema);
+
+type InstitutionModelType = Model<InstitutionDocument>;
 
 export { InstitutionModel, InstitutionModelType };
