db6f0c9ebf6a0ce6aec78fe3f186f2ba6eb1d17c
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-18 08:19:57 -0300

    test fix: get-employees-by-institution-id usersDaoMock with return mock value

--- src/institutions/usecases/get-employees-by-institution-id/get-employees-by-institution-id.spec.ts
+++ src/institutions/usecases/get-employees-by-institution-id/get-employees-by-institution-id.spec.ts
@@ -4,27 +4,27 @@ import { UsersDaoResult } from "../../../data/users/daos";
 type SutParams = { institutionId: string };
 type SutType = { institutionId: string; sut: GetEmployeeByInstitutionIdResult };
 
-const mockDao: UsersDaoResult = {
+const usersDaoMock: UsersDaoResult = {
     findEmployeesByInstitutionId: jest.fn(),
     findById: jest.fn(),
     findStudentsByInstitutionId: jest.fn(),
 };
 
 const makeSut = ({ institutionId }: SutParams): SutType => {
-    const sut = new GetEmployeesByInstitutionId(institutionId, mockDao);
+    const sut = new GetEmployeesByInstitutionId(institutionId, usersDaoMock);
     return { sut, institutionId };
 };
 
 describe("get-employees-by-institution-id needs to return a list of employees related to the institution", () => {
     it("should throw exception cause the institution has no employees. Thats mandatory.", async () => {
         const { sut } = makeSut({ institutionId: "NOT_EXISTS" });
-
+        usersDaoMock.findStudentsByInstitutionId = jest.fn().mockReturnValue([]);
         expect(async () => await sut.perform()).rejects.toThrow(`The institutionId NOT_EXISTS has no employees.`);
     });
 
     it("should return the filled list of employees", async () => {
         const { sut } = makeSut({ institutionId: "existant_institution" });
-        mockDao.findEmployeesByInstitutionId = jest.fn().mockReturnValue([{}, {}]);
+        usersDaoMock.findEmployeesByInstitutionId = jest.fn().mockReturnValue([{}, {}]);
         const employees = await sut.perform();
         expect(employees.length).toBeGreaterThanOrEqual(1);
     });
