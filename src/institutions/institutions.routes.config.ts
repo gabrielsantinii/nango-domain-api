36f2eb8a8acfa6d9b572d33c66a0812de7d19e0e
Gabriel Santini <gabrielgsantini@gmail.com>
2021-12-18 22:51:51 -0300

    feat: routing for institution-creation flow

--- src/institutions/institutions.routes.config.ts
+++ src/institutions/institutions.routes.config.ts
@@ -1,14 +1,26 @@
 import { Application } from "express";
 import { SharedRoutesConfig } from "../shared";
-import { MountDashboardController, CreateInstitutionController } from "./controllers";
-import { GetInstitutionByIdMiddleware, ValidateNeededInstitutionId, ValidateInstitutionFields } from "./middlewares";
+import { ResultValidationMiddleware } from "../shared/middlewares";
+import { MountDashboardController } from "./controllers";
+import {
+    GetInstitutionByIdMiddleware,
+    ValidateNeededInstitutionId,
+    ValidateInstitutionFields,
+    CreateInstitutionMiddleware,
+    CreateUserMiddleware,
+    CreateAuthMiddleware,
+} from "./middlewares";
 
 const mountDashboardController = new MountDashboardController();
 const getInstitutionByIdMiddleware = new GetInstitutionByIdMiddleware();
 const validateNeededInstitutionId = new ValidateNeededInstitutionId();
 
 const validateInstitutionFields = new ValidateInstitutionFields();
-const createInstitutionController = new CreateInstitutionController();
+const createInstitution = new CreateInstitutionMiddleware();
+const createAuth = new CreateAuthMiddleware();
+const createUser = new CreateUserMiddleware();
+
+const resultValidationMiddleware = new ResultValidationMiddleware();
 
 export class InstitutionsRoutesConfig extends SharedRoutesConfig {
     constructor(app: Application) {
@@ -18,7 +30,10 @@ export class InstitutionsRoutesConfig extends SharedRoutesConfig {
     configureRoutes(): Application {
         this.app.post(`${this.path}`, [
             ...validateInstitutionFields.getValidationList(),
-            createInstitutionController.perform,
+            resultValidationMiddleware.verifyAndReturnErrors,
+            createInstitution.perform,
+            createAuth.perform,
+            createUser.perform,
         ]);
 
         this.app.get(`${this.path}/:institutionId/dashboard`, [
